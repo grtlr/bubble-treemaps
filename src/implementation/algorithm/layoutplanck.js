60fca0631e4c33683bcea025033bdedd5fe4f4dd
TLow92 <thilo.spinner@uni-konstanz.de>
2017-09-25 13:14:00 +0200

    BUGFIX: Attract circle bodies to body cluster center instead of image center.

--- src/implementation/algorithm/layoutplanck.js
+++ src/implementation/algorithm/layoutplanck.js
@@ -1,6 +1,25 @@
 import * as planck from 'planck-js';
 import getLayerClusters from './getlayerclusters';
 
+// Extend array prototype by unique function.
+Array.prototype.contains = function(v) {
+    for(var i = 0; i < this.length; i++) {
+        if(this[i] === v) return true;
+    }
+    return false;
+};
+
+Array.prototype.unique = function() {
+    var arr = [];
+    for(var i = 0; i < this.length; i++) {
+        if(!arr.contains(this[i])) {
+            arr.push(this[i]);
+        }
+    }
+    return arr;
+};
+
+
 export default function(hierarchyRoot, padding, width, height) {
 
     // Circle pack by d3.
@@ -13,12 +32,35 @@ export default function(hierarchyRoot, padding, width, height) {
     for(let layerDepth = hierarchyRoot.height - 1; layerDepth >= 0; layerDepth--) {
         // Get clusters of circles on this layer.
         let layerClusters = getLayerClusters(hierarchyRoot, layerDepth, padding);
+
+        // Sort clusters by parents parent, to set correct center of attraction for bodies.
+        let pps = [];
+        layerClusters.forEach(function(cluster) {
+            pps.push(cluster.parent.parent);
+        });
+        pps = pps.unique();
+
         // Do the layout.
-        layoutClusters(layerClusters, width, height);
+        pps.forEach(function(pp) {
+            let currentPPClusters = layerClusters.filter(function(cluster) {
+                return cluster.parent.parent === pp;
+            });
+
+            let circleList = [];
+            currentPPClusters.forEach(function(cluster) {
+                circleList = circleList.concat(cluster.nodes);
+            });
+
+            let centroid = getCircleCentroid(circleList);
+
+            console.log(layerClusters);
+            console.log(currentPPClusters);
+            layoutClusters(currentPPClusters, centroid);
+        });
     }
 }
 
-function layoutClusters(layerClusters, width, height) {
+function layoutClusters(layerClusters, centroid) {
     // Create world with zero gravity.
     let world = planck.World({
         gravity: planck.Vec2(0,0)
@@ -31,7 +73,7 @@ function layoutClusters(layerClusters, width, height) {
     });
 
     // Create attractor.
-    let attractorBody = world.createBody(planck.Vec2(width/2,height/2));
+    let attractorBody = world.createBody(planck.Vec2(centroid.x, centroid.y));
 
     // Create joints between layerClusterBodies and attractor.
     layerClusterBodies.forEach(function(layerClusterBody) {
@@ -78,18 +120,8 @@ function layoutClusters(layerClusters, width, height) {
 }
 
 function createClusterBody(layerCluster, world) {
-    // Calculate centroid of circle group.
-    let circleMassSum = 0;
-    let bodyCentroid = planck.Vec2.zero();
-
-    layerCluster.nodes.forEach(function(circle) {
-        let circleMass = circle.r * circle.r * Math.PI;
-        circleMassSum += circleMass;
-        bodyCentroid.x += circle.x * circleMass;
-        bodyCentroid.y += circle.y * circleMass;
-    });
-
-    bodyCentroid.mul(1.0/circleMassSum);
+    // Get centroid of all circles.
+    let bodyCentroid = getCircleCentroid(layerCluster.nodes);
 
     // Create body.
     let body = world.createDynamicBody(bodyCentroid);
@@ -109,4 +141,21 @@ function createClusterBody(layerCluster, world) {
 
     // Return completed body.
     return body;
+}
+
+function getCircleCentroid(circles) {
+    // Calculate centroid of circle group.
+    let circleMassSum = 0;
+    let centroid = planck.Vec2.zero();
+
+    circles.forEach(function(circle) {
+        let circleMass = circle.r * circle.r * Math.PI;
+        circleMassSum += circleMass;
+        centroid.x += circle.x * circleMass;
+        centroid.y += circle.y * circleMass;
+    });
+
+    centroid.mul(1.0/circleMassSum);
+
+    return centroid;
 }
