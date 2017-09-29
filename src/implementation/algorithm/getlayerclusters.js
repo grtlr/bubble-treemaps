5568e4dccf01871ba1bca61920ad0fd860e4b978
TLow0 <thilo.spinner@uni-konstanz.de>
2017-09-29 17:44:25 +0200

    Bugfix (padding between groups dynamic).

--- src/implementation/algorithm/getlayerclusters.js
+++ src/implementation/algorithm/getlayerclusters.js
@@ -24,7 +24,7 @@ export default function(hierarchyRoot, layerDepth, padding) {
             let contourClusterParentUncertainty = clusterParent.uncertainty/2;                                // Padding for contour (contour lies 50% outside of parent node).
             let planckClusterParentUncertainty = node !== clusterParent ? clusterParent.uncertainty : 0;      // Padding for force based layout (contours should not cut each other, i.e. full parent contour should be taken into account).
 
-            let interClusterSpacing = clusterNodes.length === 1 ? 0 : 5; // Add constant padding for single circle in group.
+            let interClusterSpacing = clusterNodes.length === 1 ? 0 : padding / 2.0; // For single circle: No padding, since it has no contour.
 
             node.contourPadding = (node.depth - clusterParent.depth) * padding + uncertaintySum + contourClusterParentUncertainty;
             node.planckPadding = (node.depth - clusterParent.depth) * padding + uncertaintySum + planckClusterParentUncertainty + interClusterSpacing;
